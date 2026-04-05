a6ffd2eddeb3315b416a84be04a25996d7437785
gummybearansh <gummybearansh@gmail.com>
2026-04-05 17:55:00 +0530

    feat: added block parsing of 6 block types - heading, paragraph, quote, ordered-list, unordered-list, code-block as well as converstion to nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 import re
+
 from textnode import TextNode, TextType
 
 
@@ -11,7 +12,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         node_split = node.text.split(delimiter)
 
         # don't hardcode it to 3 -> string can have multiple delimiter sections
-        if len(node_split) % 2 == 0: 
+        if len(node_split) % 2 == 0:
             raise SyntaxError("Invalid Markdown Syntax")
 
         this_node = []
@@ -21,7 +22,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 continue
             if i % 2 != 0:
                 this_node.append(TextNode(node_split[i], text_type))
-            else: 
+            else:
                 this_node.append(TextNode(node_split[i], TextType.TEXT))
 
         new_nodes.extend(this_node)
@@ -41,6 +42,72 @@ def extract_markdown_links(text):
     return matches
 
 
-def split_nodes_images(old_nodes):
-    pass
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown syntax")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            # exactly what i thought of doing, once i've split the first link i found, and the text before it, i can now consider the rest of the string as the original string and perform the same op on it
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown syntax")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    delimiters = [
+        ("`", TextType.CODE),
+        ("**", TextType.BOLD),
+        ("*", TextType.ITALIC),
+        ("_", TextType.ITALIC),
+    ]
+
+    for delimiter, text_type in delimiters:
+        nodes = split_nodes_delimiter(nodes, delimiter, text_type)
+
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+
+    return nodes
 
